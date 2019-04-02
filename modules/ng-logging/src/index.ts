8cc71b1f4a7df94614f6efbdc6538e33653f5f13
Moe Myat Zaw <mmzliveid@gmail.com>
2019-04-02 17:48:08 +0630

    Initial commit

---
+++ modules/ng-logging/src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @module
+ * @description
+ * Entry point from which you should import all public core APIs.
+ */
+
+export * from './logger';
+export * from './logger-factory';
+export * from './logger-provider';
+export * from './log-level';
+export { LoggerModule } from './logger.module';
+export { ConsoleLoggerModule } from './console-logger.module';
