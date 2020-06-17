f795cab36c6aeec7380daa47ce453837bdf9fe56
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-17 15:17:20 +0630

    Update demo app

--- samples/demo-app/src/app/app.component.ts
+++ samples/demo-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
 
 import { LogService, Logger } from '@dagonmetric/ng-log';
 
@@ -8,32 +8,47 @@ import { LogService, Logger } from '@dagonmetric/ng-log';
     styleUrls: ['./app.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnDestroy {
     private readonly logger: Logger;
 
     constructor(private readonly logService: LogService) {
         // Create a child logger named 'AppComponent'
-        this.logger = this.logService.createLogger(AppComponent.name);
+        this.logger = this.logService.createLogger('app-logger');
     }
 
-    ngOnInit(): void {
-        // Track traces
+    logTrace(): void {
         this.logger.trace('Testing trace');
+    }
+
+    logDebug(): void {
         this.logger.debug('Testing debug');
+    }
+
+    logInfo(): void {
         this.logger.info('Testing info');
-        // this.logger.warn('Testing warn');
+    }
 
-        // Track exceptions
-        // this.logger.error(new Error('Testing error'));
-        // this.logger.fatal(new Error('Testing critical'));
+    logWarn(): void {
+        this.logger.warn('Testing warn');
+    }
 
+    logError(): void {
+        this.logger.error(new Error('Testing error'));
+    }
+
+    logFatal(): void {
+        this.logger.fatal(new Error('Testing critical'));
+    }
+
+    trackPageView(): void {
         // Track page view
         this.logger.trackPageView({
             name: 'My Angular App',
             uri: '/home'
         });
+    }
 
-        // Track custom event
+    trackEvent(): void {
         this.logger.trackEvent({
             name: 'video_auto_play_start',
             properties: {
