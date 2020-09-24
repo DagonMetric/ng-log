6ab4b31d40ca74bc2801d3977858d27cf034d4d2
Moe Myat Zaw <mmzliveid@gmail.com>
2020-09-25 00:03:10 +0630

    Update test.ts

--- modules/ng-log/test.ts
+++ modules/ng-log/test.ts
@@ -2,7 +2,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable import/no-extraneous-dependencies */
 
 import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
