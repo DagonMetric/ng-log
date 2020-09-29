d433a9e2250a769c324eddb06b4d5b1dfe5361e9
Moe Myat Zaw <mmzliveid@gmail.com>
2020-09-29 22:56:41 +0630

    Update index.ts

--- modules/ng-log/src/index.ts
+++ modules/ng-log/src/index.ts
@@ -4,12 +4,15 @@
  * Entry point from which you should import all public core APIs.
  */
 
+export * from './default-logger';
 export * from './event-info';
 export * from './log-config';
 export * from './log-info';
 export * from './log-level';
 export * from './log.module';
 export * from './log.service';
+export * from './logger-filter-options';
+export * from './logger-information';
 export * from './logger-provider';
 export * from './logger';
 export * from './page-view-info';
